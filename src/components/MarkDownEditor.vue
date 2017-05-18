53a60868221851a2ff2d1efa9fb41b4d90100b72
tt36999 <tt36999@hotmail.com>
2017-05-18 17:30:15 +0800

    整合树形图和MarkDown

---
+++ src/components/MarkDownEditor.vue
@@ -0,0 +1,12 @@
+<template>
+    <textarea :value="rowText" name="rowText" @inpu="update"></textarea>
+    <div v-html="getMarkDown"></div>
+</template>
+<script>
+    export defalut{
+        name:'MarkDownEditor'
+    }
+</script>
+<style>
+    
+</style>
